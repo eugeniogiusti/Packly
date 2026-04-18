607f1c7366353fb514365bbaaa3e7b487289f707
eugeniogiusti <eugenio.giusti@outlook.it>
2026-04-18 13:38:14 +0200

    tolte policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        // Super-admin/Mono-utente bypass: permette qualsiasi azione a prescindere dalle Policies
+        Gate::before(function ($user, $ability) {
+            return true;
+        });
     }
 }
